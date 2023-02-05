a7c9d028afc767d6bd92f6dcf65670082df814ef
luisgustavom1 <luisgustavomacedo13@gmail.com>
2023-02-05 11:29:46 -0300

    feat: add a binary search tree

---
+++ trees/implementation/bst.go
@@ -0,0 +1,45 @@
+package implementation
+
+import (
+	"trees/shared/node"
+)
+
+type BinarySearchTree struct {
+	Root *node.Node
+}
+
+func NewBinarySearchTree() *BinarySearchTree {
+	return &BinarySearchTree{
+		Root: nil,
+	}
+}
+
+func (bst *BinarySearchTree) AddNode(value int) {
+	newNode := node.NewNode(value)
+
+	if bst.Root == nil {
+		bst.Root = newNode
+		return
+	}
+	
+	var currentNode *node.Node = bst.Root
+
+	for {
+		if value < currentNode.Value {
+			if currentNode.Left == nil {
+				currentNode.Left = newNode
+				return
+			}
+
+			currentNode = currentNode.Left
+			continue
+		}
+
+		if currentNode.Right == nil {
+			currentNode.Right = newNode
+			return
+		}
+
+		currentNode = currentNode.Right
+	}
+}
