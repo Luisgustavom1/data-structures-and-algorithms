e9b3ece883c7175ebd4fd87c7cbc8234c083b0bd
luisgustavom1 <luisgustavomacedo13@gmail.com>
2023-02-05 11:30:00 -0300

    feat: testing the bst

---
+++ trees/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"trees/implementation"
+)
+
+func main() {
+	bst := implementation.NewBinarySearchTree()
+
+	bst.AddNode(8)
+	bst.AddNode(3)
+	bst.AddNode(1)
+	bst.AddNode(6)
+	bst.AddNode(4)
+	bst.AddNode(10)
+	bst.AddNode(14)
+	bst.AddNode(13)
+	bst.AddNode(7)
+
+	fmt.Printf("       %d\n", bst.Root.Value)
+
+	fmt.Println("     /   \\ ")
+	fmt.Printf("    %d", bst.Root.Left.Value)
+	fmt.Printf("     %d\n", bst.Root.Right.Value)
+
+	fmt.Println("   / \\     \\ ")
+	fmt.Printf("  %d", bst.Root.Left.Left.Value)
+	fmt.Printf("   %d", bst.Root.Left.Right.Value)
+
+	fmt.Printf("    %d\n", bst.Root.Right.Right.Value)
+
+	fmt.Println("     / \\   /")
+	fmt.Printf("    %d", bst.Root.Left.Right.Left.Value)
+	fmt.Printf("   %d", bst.Root.Left.Right.Right.Value)
+
+	fmt.Printf("  %d", bst.Root.Right.Right.Left.Value)
+}
