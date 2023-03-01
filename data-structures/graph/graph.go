e09c4de2c31100ac8e1f05041b363bd6ff802445
luisgustavom1 <luisgustavomacedo13@gmail.com>
2023-03-01 07:31:47 -0300

    feat: implements graph and BreadthFirstSearch

---
+++ data-structures/graph/graph.go
@@ -0,0 +1,41 @@
+package graph
+
+import (
+	"data-structures/queue"
+	"data-structures/shared/array"
+)
+
+type Graph struct {
+	Elements map[string][]string
+}
+
+func NewGraph() *Graph {
+	return &Graph{
+		Elements: make(map[string][]string),
+	}
+}
+
+func (graph *Graph) BreadthFirstSearch(value string) bool {
+	queueSearch := queue.NewQueue(graph.Elements["voce"]...)
+	visitedElements := []string{}
+
+	for len(queueSearch.Value) > 0 {
+		element, err := queueSearch.Dequeue()
+		if err != nil {
+			return false
+		}
+
+		if element == value {
+			return true
+		}
+
+		if array.Includes(visitedElements, element) {
+			continue
+		}
+
+		visitedElements = append(visitedElements, element)
+		queueSearch.Enqueue(graph.Elements[element]...)
+	}
+
+	return false
+}
