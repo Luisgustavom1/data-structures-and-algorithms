841201e820807f905272b04f190e491e6aae7377
luisgustavom1 <luisgustavomacedo13@gmail.com>
2023-08-04 10:27:19 -0300

    refactor: turn Queue generic

--- data-structures/queue/queue.go
+++ data-structures/queue/queue.go
@@ -1,33 +1,25 @@
 package queue
 
-import (
-	"errors"
-)
-
-type Queue struct {
-	Value []string
+type Queue[T any] struct {
+	Value []T
 }
 
-func NewQueue(values ...string) *Queue {
-	return &Queue{
+func NewQueue[T any](values ...T) *Queue[T] {
+	return &Queue[T]{
 		Value: values,
 	}
 }
 
-func (queue *Queue) Enqueue(value ...string) bool {
+func (queue *Queue[T]) Enqueue(value ...T) bool {
 	queue.Value = append(queue.Value, value...)
 
 	return true
 }
 
-func (queue *Queue) Dequeue() (value string, err error) {
-	if len(queue.Value) == 0 {
-		return "", errors.New("queue is empty")
-	}
-
+func (queue *Queue[T]) Dequeue() (value T, err error) {
 	value = queue.Value[0]
 
-	queueCopy := make([]string, len(queue.Value)-1)
+	queueCopy := make([]T, len(queue.Value)-1)
 
 	copy(queueCopy, queue.Value[1:])
 	queue.Value = queueCopy
