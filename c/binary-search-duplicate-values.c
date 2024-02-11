7d387f1c12b92d8102468d12b823cfc14271d500
Luis Gustavo <luisgustavomacedo13@gmail.com>
2024-02-11 19:58:38 -0300

    correct binary search with duplicate values

--- c/binary-search-duplicate-values.c
+++ c/binary-search-duplicate-values.c
@@ -1,38 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* binarySearchDuplicateValues(int n, int* vector, int size) {
-	int* r; 
-	r = malloc(0);
-	int rs = 0;	
+int* binarySearchDuplicateValues(int n, int* vector, int size, int* rs) {
+	int* r;
+	r = malloc(1 * sizeof(4));
+	*rs = 0;
+	int lb = -1;
 	int s = 0;
 	int e = size - 1; 
 	while(s <= e) {
-		int m = (s + e) / 2;
-		if (n < vector[m]) {
+		int m = s + (e - s) / 2;
+		if (vector[m] >= n) {
 			e = m - 1;
-		} else if (n > vector[m]) {
-			s = m + 1;
 		} else {
-			r = realloc (r, 1 * sizeof(int));
-			r[rs] = m;
-			rs++;
-			s = 2*m + 2 - e;
-			e = 2*m + 2 - s;
+			lb = m + 1;
+			s = m + 1;
 		}
 	}
 	
+	int temp = vector[lb];
+	while(temp == n) {
+		r = realloc (r, 1 * sizeof(int));
+		r[*rs] = lb;
+		*rs = *rs + 1;
+		lb++;
+		temp = vector[lb];
+	}
+
 	return r;
 }
 
 int main() {
 	int vector[] = {2, 3, 6, 6, 7, 8, 9, 34, 34, 56, 37, 73, 73};
 	int size = sizeof(vector)/sizeof(int);
-	int* results = binarySearchDuplicateValues(73, vector, size);
-	int ps = sizeof(results); 
-	for (int i = 0; i < (ps / sizeof(results[0])); i++) {
-		printf("result -> %d\n", results[i]);
+   	int rs;
+	int* r = binarySearchDuplicateValues(73, vector, size, &rs);
+	for (int i = 0; i < rs; i++) {
+		printf("result -> %d\n", r[i]);
 	}
+	free(r);
 	
 	return 0;
 }
