6a708682202bfa22b1611f41bb8a624c466a1d6f
Luis Gustavo <luisgustavomacedo13@gmail.com>
2024-02-17 10:31:50 -0300

    bubble sort

---
+++ c/sort/bubble-sort.c
@@ -0,0 +1,36 @@
+#include <stdio.h>                                                                                                                                                                                          
+	
+void bubbleSort(int* v, int n) {
+	int temp, s, i;	
+	int e = n;
+
+	do {
+		s = 0;
+		for (i = 0; i < e - 1; i++) {
+			if (v[i] > v[i + 1]) {
+				temp = v[i];
+				v[i] = v[i + 1];
+				v[i + 1] = temp;
+				s = 1;
+			}
+		}
+		e--;
+	} while (s == 1);	
+}
+
+int main() {
+	int v[] = {25, 7, 8, 67, 1, 99};
+	int s = sizeof(v) / sizeof(int);
+	for (int i = 0; i < s; i++) {
+		printf("%d - %d\n", i + 1, v[i]);
+	}
+
+	bubbleSort(v, s);
+	
+	printf("Ordered:\n");
+	for (int i = 0; i < s; i++) {
+		printf("%d - %d\n", i + 1, v[i]);
+	}
+
+	return 0;
+}
