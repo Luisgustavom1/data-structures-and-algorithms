7953c6a8643a958ad7c5de2d42846bcb5dcb8955
Luis Gustavo <luisgustavomacedo13@gmail.com>
2024-03-10 09:45:14 -0300

    linked_list header

---
+++ c/linked-list/linked-list.h
@@ -0,0 +1,10 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+external void print(Node *node);
+external void insert(Node *node, int n);
+external Node* init(int n);
+external Node* search(Node *node, int n);
+external void removeByValue(Node *node, int n);
+
+#endif
