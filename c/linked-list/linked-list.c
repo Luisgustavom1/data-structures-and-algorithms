d95e50a1e3a6df58e52bd573abcc47dea0aefa00
Luis Gustavo <luisgustavomacedo13@gmail.com>
2024-03-12 08:44:33 -0300

    remove queue methods implementations

--- c/linked-list/linked-list.c
+++ c/linked-list/linked-list.c
@@ -2,12 +2,10 @@
 #include <stdlib.h>
 #include "linked-list.h"
 
-void printQueue(Node *head) {
-	Node *tmp = head->next;
-	do {
-		printf("%d\n", tmp->value);
-		tmp = tmp->next;
-	} while (tmp != head->next);
+Node* LinkedList() {
+	Node *head = malloc(sizeof(Node));
+	head->next = NULL;
+	return head;
 }
 
 void print(Node *head) {
@@ -16,19 +14,6 @@ void print(Node *head) {
 		printf("%d\n", tmp->value);
 		tmp = tmp->next;
 	};
-}
-
-Node* Queue(int n) {
-	Node *head = malloc(sizeof(Node));
-	head->value = n;
-	head->next = head;
-	return head;
-}
-
-Node* LinkedList() {
-	Node *head = malloc(sizeof(Node));
-	head->next = NULL;
-	return head;
 }
 
 Node* search(Node *head, int n) {
@@ -61,51 +46,23 @@ Node* append(Node *head, int n) {
 	return new_node;
 }
 
-Node* enqueue(Node *head, int n) {
-	Node *new_node;
-	new_node = malloc(sizeof(Node));	
-	new_node->value = n;
-	new_node->next = head->next;
-	head->next = new_node;
-	return new_node;
-}
-
-int dequeue(Node *head) {
-	if (head == NULL) return -1;
-	Node *tmp = head->next;
-	int v = tmp->value;
-	head->next = tmp->next;
-	free(tmp);
-	return v;
-}
-
-int main() {
-	Node *linkedList = LinkedList(); // [7 | ] -> [7 | ]
-	append(linkedList, 7);
-	append(linkedList, 19);
-	append(linkedList, 18);
-	append(linkedList, 17);
-	append(linkedList, 16);
-	append(linkedList, 15);
-	// queue = enqueue(queue, 3); // [3 | ] -> [7 | ] -> [3 | ]
-	// queue = enqueue(queue, 8); // [8 | ] -> [7 | ] -> [3 | ] -> [8 | ]
-	// queue =	enqueue(queue, 19); 
-	// queue = enqueue(queue, 1);
+// int main() {
+// 	Node *linkedList = LinkedList(); // [7 | ] -> [7 | ]
+// 	append(linkedList, 7);
+// 	append(linkedList, 19);
+// 	append(linkedList, 18);
+// 	append(linkedList, 17);
+// 	append(linkedList, 16);
+// 	append(linkedList, 15);
 	
-	print(linkedList);
-	removeByValue(linkedList, 17);
-	Node *t = search(linkedList, 17);
-	if (t == NULL) {
-		printf("not found\n");
-	} else {
-		printf("found %d\n", t->value);
-	}
-	// printf("====\n");
-	// printf("f %d\n", dequeue(queue));
-	// printf("f %d\n", dequeue(queue));
-	// printf("f %d\n", dequeue(queue));
-	// printf("f %d\n", dequeue(queue));
-	// printf("f %d\n", dequeue(queue));
+// 	print(linkedList);
+// 	removeByValue(linkedList, 17);
+// 	Node *t = search(linkedList, 17);
+// 	if (t == NULL) {
+// 		printf("not found\n");
+// 	} else {
+// 		printf("found %d\n", t->value);
+// 	}
 	
-	return 0;
-}
+// 	return 0;
+// }
