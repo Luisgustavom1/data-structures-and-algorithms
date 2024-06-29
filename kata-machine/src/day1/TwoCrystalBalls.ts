c6c62399d513649d3a526a11ffc4b2137f9bf6eb
Luis Gustavo <luisgustavomacedo13@gmail.com>
2024-06-29 09:36:48 -0300

    TwoCrystalBalls

--- kata-machine/src/day1/TwoCrystalBalls.ts
+++ kata-machine/src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,20 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+	let jmp = Math.floor(Math.sqrt(breaks.length));
 
+	let i = jmp;
+	for (; i < breaks.length; i+=jmp) {
+		if (breaks[i]) {
+			break;
+		}
+	}
+
+	i-=jmp;
+
+	for (; i < breaks.length; i++) {
+		if (breaks[i]) {
+			return i;
+		}
+	}
+
+	return -1;
 }
