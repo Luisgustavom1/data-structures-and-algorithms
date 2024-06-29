87a002c744c4c53f4a65ef5f12782036e924feb6
Luis Gustavo <luisgustavomacedo13@gmail.com>
2024-06-29 09:10:23 -0300

    kata-machine

---
+++ kata-machine/src/day1/LinearSearchList.ts
@@ -0,0 +1,6 @@
+export default function linear_search(haystack: number[], needle: number): boolean {
+	for (let i = 0; i < haystack.length; i++) {
+		if (haystack[i] === needle) return true;
+	}
+	return false;
+}
