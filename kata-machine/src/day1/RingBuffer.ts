dddea87cac0126fc2e4565da894a467def333a5c
Luis Gustavo <luisgustavomacedo13@gmail.com>
2024-06-30 21:25:34 -0300

    ring bufffer

---
+++ kata-machine/src/day1/RingBuffer.ts
@@ -0,0 +1,13 @@
+export  default class RingBuffer<T> {
+  constructor() {}
+
+  push(item: T): void {}
+
+  pop(): T | undefined {
+    return undefined;
+  }
+
+  get(idx: number): T | undefined {
+    return undefined;
+  }
+}
