5603292bdcb2205cbc7313d973ebcdfc907d283a
Luis Gustavo <luisgustavomacedo13@gmail.com>
2024-07-01 21:20:02 -0300

    queue

--- kata-machine/src/day1/Queue.ts
+++ kata-machine/src/day1/Queue.ts
@@ -1,18 +1,48 @@
+type Node<T> = {
+    value: T,
+    next?: Node<T>,
+}
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head: Node<T> | undefined;
+    private tail: Node<T> | undefined
 
     constructor() {
+        this.tail = this.head = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
+        this.length++;
+        const node: Node<T> = { value: item };
+        if (!this.tail) {
+            this.head = this.tail = node;
+            return;
+        }
 
-}
-    deque(): T | undefined {
+        this.tail.next = node;
+        this.tail = node;
+        return;
+    }
 
-}
+    dequeue(): T | undefined {
+        if (!this.head) return undefined;
+
+        this.length--;
+        
+        const head = this.head;
+        this.head = this.head?.next
+
+        // free
+        head.next = undefined;
+
+        if (this.length === 0) this.head = this.tail = undefined;
+
+        return head.value;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
