5f7475fa6082a684af464286aeceb437c7117bf3
Luis Gustavo <luisgustavomacedo13@gmail.com>
2024-07-11 08:05:30 -0300

    breadth first search

--- kata-machine/src/day1/BTBFS.ts
+++ kata-machine/src/day1/BTBFS.ts
@@ -1,3 +1,17 @@
-export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+import Queue from "./Queue";
 
+export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+  if (head.value === needle) return true;
+
+  const queue = new Queue<BinaryNode<number>>();
+  queue.enqueue(head)
+
+  while (queue.length > 0) {
+    const n = queue.dequeue();
+    if (n?.value === needle) return true;
+    if (n?.left) queue.enqueue(n.left);
+    if (n?.right) queue.enqueue(n.right);
+  }
+
+  return false;
 }
