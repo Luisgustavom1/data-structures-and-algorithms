9198208b86032ba8687174b0a56bbf633e22d3a7
Luis Gustavo <luisgustavomacedo13@gmail.com>
2024-07-17 08:09:27 -0300

    bfs graph matrix

--- kata-machine/src/day1/BFSGraphMatrix.ts
+++ kata-machine/src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,38 @@
-export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+import Queue from "./Queue";
 
+export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+  const seen = new Array(graph.length).fill(false);
+  const prev = new Array(graph.length).fill(-1);
+
+  seen[source] = true;
+  const q = new Queue<number>();
+  q.enqueue(source);
+
+  do {
+    const curr = q.dequeue()!;
+    if (needle === curr) {
+      break;
+    }
+
+    const adjs = graph[curr];
+    for (let i = 0; i < adjs.length; i++) {
+      if (adjs[i] === 0 || seen[i]) continue;
+      seen[i] = true;
+      prev[i] = curr;
+      q.enqueue(i);
+    }
+    
+    seen[curr] = true;
+  } while(q.length);
+
+  let curr = needle;
+  const out = [];
+  while (prev[curr] !== -1) {
+    out.push(curr)
+    curr = prev[curr];
+  }
+
+  if (!out.length) return null;
+
+  return [source].concat(out.reverse());
 }
