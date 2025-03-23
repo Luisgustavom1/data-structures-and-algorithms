10333b5114103462cb4d3eb5245d56339da613e9
Luis Gustavo <luisgustavomacedo13@gmail.com>
2025-03-23 17:23:29 -0300

    fix journey.py

--- py/journey.py
+++ py/journey.py
@@ -1,38 +1,35 @@
 # https://codeforces.com/problemset/problem/839/C
 c = int(input())
 
+if (c == 1):
+    print("0.000000")
+    exit()
+
 graph = [[] for _ in range(c + 1)]
-leafs = []
 
 for i in range(c - 1):
     n, k = map(int, input().split())
     graph[n].append(k)
-
-for i in range(c + 1):
-    if (len(graph[i]) == 0 and i > 0):
-        leafs.append(i)
+    graph[k].append(n)
 
 visited = [False] * (c+1)
-distances = [0] * (c + 1)
+def dfs(root):
+    total = 0
+    count = 0
 
-q = [(1, 0)]
-while (len(q) > 0):
-    ad = q.pop()
-    a = ad[0]
-    d = ad[1]
-    
-    if (a == c):
-        continue
-    
-    nex = graph[a]
-    for n in nex:
-        if (n not in visited):
-            visited[n] = True
-            q.append((n, d+1))
-            distances[n] = d+1
+    visited[root] = True
 
-s = 0
-for p in leafs:
-    s = s + distances[p]
+    for node in graph[root]:
+        if (not visited[node]):
+            count += 1;
+            total += dfs(node) + 1
 
-print(f"{(s / len(leafs)):.6f}") 
+    visited[root] = False
+
+    if (count == 0):
+        return 0
+
+    return total / count
+
+res = dfs(1)
+print(f"{res:.6f}") 
