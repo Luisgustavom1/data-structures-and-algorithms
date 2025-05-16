60792bc151bf4ff09fb785bc1e8e229ab14f8ee0
Luis Gustavo <luisgustavomacedo13@gmail.com>
2025-05-16 09:49:42 -0300

    monty hall problem

---
+++ go/monte-carlo-simulation/monty-hall-problem.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	var (
+		iterations = 1_000_000
+		success    = 0
+	)
+
+	for i := 0; i < iterations; i++ {
+		prize := rand.Intn(3) + 1
+		firstChoice := rand.Intn(3) + 1
+		doorOpened := rand.Intn(3) + 1
+
+		for doorOpened == firstChoice || doorOpened == prize {
+			doorOpened = rand.Intn(3) + 1
+		}
+
+		// The second choice is the remaining door
+		secondChoice := 6 - firstChoice - doorOpened
+
+		if secondChoice == prize {
+			success++
+		}
+	}
+
+	prob := float64(success) / float64(iterations)
+	expected := 2.0 / 3.0
+	errorPct := 100.0 * (prob / expected)
+	fmt.Printf("Success: %9f%%\n", prob*100)
+	fmt.Printf("Failure: %9f%%\n", errorPct)
+}
